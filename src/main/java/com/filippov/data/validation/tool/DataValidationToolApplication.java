e2c367881808baf0ba2f033bf0573b64613caf4a
Alex Filippov <a.filippov@spd-ukraine.com>
2018-11-15 19:30:06 +0200

    initial commit

---
+++ src/main/java/com/filippov/data/validation/tool/DataValidationToolApplication.java
@@ -0,0 +1,12 @@
+package com.filippov.data.validation.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataValidationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataValidationToolApplication.class, args);
+	}
+}
