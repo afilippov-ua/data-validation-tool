cfb8d3dcdef1e5063237f3c6395e21ac10da77e8
Alex Filippov <a.filippov@spd-ukraine.com>
2019-01-18 15:35:51 +0200

    Datasource metadata test coverage was extended

--- data-validation-framework/src/test/java/com/filippov/data/validation/tool/datasource/DatasourceMetadataTest.java
+++ data-validation-framework/src/test/java/com/filippov/data/validation/tool/datasource/DatasourceMetadataTest.java
@@ -1,6 +1,7 @@
 package com.filippov.data.validation.tool.datasource;
 
 import com.filippov.data.validation.tool.AbstractTest;
+import com.filippov.data.validation.tool.model.DataType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,28 +10,54 @@ import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DatasourceMetadataTest extends AbstractTest {
-    private static final DatasourceTable table1 = DatasourceTable.builder().name("table1").build();
-    private static final DatasourceTable table2 = DatasourceTable.builder().name("table2").build();
+    private static final DatasourceTable table1 = DatasourceTable.builder().name(TABLE_A).build();
+    private static final DatasourceTable table2 = DatasourceTable.builder().name(TABLE_B).build();
+
+    private static final DatasourceColumn column1 = DatasourceColumn.builder().tableName(TABLE_A).name(INTEGER_COLUMN).dataType(DataType.INTEGER).build();
+    private static final DatasourceColumn column2 = DatasourceColumn.builder().tableName(TABLE_A).name(DOUBLE_COLUMN).dataType(DataType.DOUBLE).build();
+    private static final DatasourceColumn column3 = DatasourceColumn.builder().tableName(TABLE_B).name(INTEGER_COLUMN).dataType(DataType.INTEGER).build();
+    private static final DatasourceColumn column4 = DatasourceColumn.builder().tableName(TABLE_B).name(DOUBLE_COLUMN).dataType(DataType.DOUBLE).build();
 
     private static final DatasourceMetadata metadata = DatasourceMetadata.builder()
             .tables(asList(table1, table2))
+            .columns(asList(column1, column2, column3, column4))
             .build();
 
     static Object[][] tableNameProvider() {
         return new Object[][]{
-                {"table1", table1},
-                {"table2", table2}
+                {TABLE_A, table1},
+                {TABLE_B, table2}
+        };
+    }
+
+    static Object[][] columnNameProvider() {
+        return new Object[][]{
+                {TABLE_A, INTEGER_COLUMN, column1},
+                {TABLE_A, DOUBLE_COLUMN, column2},
+                {TABLE_B, INTEGER_COLUMN, column3},
+                {TABLE_B, DOUBLE_COLUMN, column4}
         };
     }
 
     @ParameterizedTest()
     @MethodSource("tableNameProvider")
     void getTableByNameTest(String columnName, DatasourceTable expectedTable) {
-        assertThat(metadata.getTableByName(columnName)).isEqualTo(expectedTable);
+        assertThat(metadata.getTableByName(columnName)).hasValue(expectedTable);
     }
 
     @Test
     void incorrectTableNameTest() {
-        assertThat(metadata.getTableByName("incorrect name")).isNull();
+        assertThat(metadata.getTableByName("incorrect name")).isEmpty();
+    }
+
+    @ParameterizedTest()
+    @MethodSource("columnNameProvider")
+    void getColumnByNameTest(String tableName, String columnName, DatasourceColumn expectedColumn) {
+        assertThat(metadata.getColumnByName(tableName, columnName)).hasValue(expectedColumn);
+    }
+
+    @Test
+    void incorrectColumnsNameTest() {
+        assertThat(metadata.getColumnByName(TABLE_A, "incorrect name")).isEmpty();
     }
 }
