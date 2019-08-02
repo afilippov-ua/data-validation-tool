28b3ad137368410570fc0d62277bca422cded1e1
Oleksandr Filippov <afilippov.ua@gmail.com>
2019-08-02 18:23:24 +0300

    File utils were udated

--- utils/src/main/java/com/filippov/data/validation/tool/FileUtils.java
+++ utils/src/main/java/com/filippov/data/validation/tool/FileUtils.java
@@ -1,30 +1,17 @@
 package com.filippov.data.validation.tool;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Comparator;
 
 public class FileUtils {
 
     public static void delete(Path directory) throws IOException {
-        if (Files.exists(directory)) {
-            Files.walkFileTree(directory, new SimpleFileVisitor<>() {
-
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    Files.delete(file);
-                    return FileVisitResult.CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                    Files.delete(dir);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        }
+        Files.walk(directory)
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
     }
 }
