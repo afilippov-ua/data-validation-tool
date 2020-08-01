547fc725a4121dbf42f6c39d0d10d0eef6617a2b
Oleksandr Filippov <afilippov.ua@gmail.com>
2020-08-01 11:43:23 +0300

    deploy script was updated

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 cd ..
-docker stop dvt
-docker rm dvt
-docker rmi dvt
+
+if [ "$(docker ps -aq -f status=running -f name=dvt)" ]; then
+	docker stop dvt
+  docker rm dvt
+	docker rmi dvt
+fi
+
 docker build -f Dockerfile -t dvt .
 docker run -d --name dvt --restart always -p 8092:8092 dvt
